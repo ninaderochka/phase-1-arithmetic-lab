a52295bd2aad0c660e0615a588548671d6b073cd
Nina Hawari <nina.hawari@gmail.com>
2022-10-17 16:49:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+Number.isInteger(random);
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = Math.max(2, 8, 16, 20);
